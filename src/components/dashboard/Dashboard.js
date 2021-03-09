fe465f7aa0a3e2358d1fc05c62d056e9472c4231
HarrisonStrand <harrisonstrand@gmail.com>
2021-03-08 16:22:55 -0800

    Add research for firebase deployment

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -36,6 +36,6 @@ const mapStateToProps = (state) => {
 export default compose(
 	connect(mapStateToProps),
 	firestoreConnect([
-		{ collection: 'posts' }
+		{ collection: 'posts', orderBy: ['createdAt', 'desc']}
 	])
 )(Dashboard)
